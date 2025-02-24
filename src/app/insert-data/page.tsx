145474779853b6b4e48d68595bf7405915825025
Thibolognaize <thibault.feat@gmail.com>
2025-02-24 17:00:51 +0100

    Ajout du formulaire d'insertion de donnée

---
+++ src/app/insert-data/page.tsx
@@ -0,0 +1,13 @@
+// src/app/insert-data/page.tsx
+import React from 'react';
+import Form from '../components/Form';
+
+const InsertDataPage = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default InsertDataPage;
